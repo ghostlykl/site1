93b15438ad8408ff16aa89479579a8cd8c24dae3
ghostlykl <103728751+ghostlykl@users.noreply.github.com>
2022-12-09 10:22:21 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -4,5 +4,93 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-</div>
+<?php Header("Content-type: text/css; charset=utf-8"); ?>
+<link rel="stylesheet" href="C:\xampp\htdocs\site\web\css\site.css">
+
+<body>
+
+    <div class="logo">
+        <h1>Название галереии</h1>
+    </div>
+
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+        </div>
+        <div class="row align-items-end">
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img" alt="Название альбома">
+                <div class="albom">Название альбома</div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+        </div>
+        <div class="row align-items-end">
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+            <div class="col">
+                <img src="images/foto.png" class="img">
+            </div>
+        </div>
+    </div>
